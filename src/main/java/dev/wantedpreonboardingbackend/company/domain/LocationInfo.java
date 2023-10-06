16fdf20f6959c818fd3802c954c948e37b8bcffc
soonhankwon <soonable@gmail.com>
2023-10-06 19:42:02 +0900

    feat : LocationInfo 생성자, Getter, EqualsAndHashCode

--- src/main/java/dev/wantedpreonboardingbackend/company/domain/LocationInfo.java
+++ src/main/java/dev/wantedpreonboardingbackend/company/domain/LocationInfo.java
@@ -1,7 +1,17 @@
 package dev.wantedpreonboardingbackend.company.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
 public class LocationInfo {
 
     private String nation;
     private String area;
+
+    public LocationInfo(String nation, String area) {
+        this.nation = nation;
+        this.area = area;
+    }
 }
