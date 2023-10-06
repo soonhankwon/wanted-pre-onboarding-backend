97c4954561536b0b321a27c4db933f69736bfcc9
soonhankwon <soonable@gmail.com>
2023-10-06 17:01:12 +0900

    test : 유저 생성자, 유효하지 않은 이메일 검증 유닛테스트(#1)

---
+++ src/test/java/dev/wantedpreonboardingbackend/user/domain/UserTest.java
@@ -0,0 +1,31 @@
+package dev.wantedpreonboardingbackend.user.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserTest {
+
+    @Test
+    @DisplayName("User NoArgs 테스트")
+    void createUser_NoArgs() {
+        User user = new User();
+        assertThat(user).isNotNull();
+    }
+
+    @Test
+    @DisplayName("User 생성자 테스트 & EqualsAndHashCode")
+    void createUser_Constructor_EqualsAndHashCode() {
+        User user = new User("abc@naver.com");
+        assertThat(user).isEqualTo(new User("abc@naver.com"));
+    }
+
+    @Test
+    @DisplayName("User 생성시 유효하지 않은 이메일 검증")
+    void validateEmail_Constructor() {
+        assertThatThrownBy(() -> new User("abcnaver.com"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
