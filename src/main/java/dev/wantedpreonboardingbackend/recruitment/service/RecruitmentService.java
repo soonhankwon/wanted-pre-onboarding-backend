46f11a2b325ddf412bce41e4f2c776db1558a640
soonhankwon <soonable@gmail.com>
2023-10-06 18:58:09 +0900

    feat : 채용공고 수정 API interface (#3)

--- src/main/java/dev/wantedpreonboardingbackend/recruitment/service/RecruitmentService.java
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment/service/RecruitmentService.java
@@ -1,8 +1,10 @@
 package dev.wantedpreonboardingbackend.recruitment.service;
 
 import dev.wantedpreonboardingbackend.recruitment.controller.dto.RecruitmentRegisterRequest;
+import dev.wantedpreonboardingbackend.recruitment.controller.dto.RecruitmentUpdateRequest;
 
 public interface RecruitmentService {
 
     void registerRecruitment(Long companyId, RecruitmentRegisterRequest dto);
+    void updateRecruitment(Long recruitmentId, Long companyId, RecruitmentUpdateRequest dto);
 }
