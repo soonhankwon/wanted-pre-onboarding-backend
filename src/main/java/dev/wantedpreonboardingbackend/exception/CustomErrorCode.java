3a732e58799d193d1705a84dffccb8f89d0480cf
soonhankwon <soonable@gmail.com>
2023-10-06 18:54:57 +0900

    feat : 채용공고 & companyId가 맞지않는 경우 에러코드 추가

--- src/main/java/dev/wantedpreonboardingbackend/exception/CustomErrorCode.java
+++ src/main/java/dev/wantedpreonboardingbackend/exception/CustomErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CustomErrorCode {
 
-    COMPANY_NOT_FOUND_INVALID_ID("회사를 DB 에서 찾을수 없습니다. companyId path 확인 필요");
+    COMPANY_NOT_FOUND_INVALID_ID("회사를 DB 에서 찾을수 없습니다. companyId path 확인 필요"),
+    RECRUITMENT_NOT_MATCHES_COMPANY_ID("채용공고와 company Id가 일치하지 않습니다. 채용공고와 companyId path 매치 확인 필요");
 
     private final String message;
 }
