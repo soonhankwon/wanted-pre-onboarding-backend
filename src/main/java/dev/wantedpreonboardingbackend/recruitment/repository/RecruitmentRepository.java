6a909e726d87409da1eec59ff033d37d310c2ecb
soonhankwon <soonable@gmail.com>
2023-10-06 20:34:58 +0900

    feat : JPQL 동적쿼리 사용 - 검색어로 검색쿼리(#6)

--- src/main/java/dev/wantedpreonboardingbackend/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment/repository/RecruitmentRepository.java
@@ -2,12 +2,15 @@ package dev.wantedpreonboardingbackend.recruitment.repository;
 
 import dev.wantedpreonboardingbackend.company.domain.Company;
 import dev.wantedpreonboardingbackend.recruitment.domain.Recruitment;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
     Optional<Recruitment> findByIdAndCompany(Long id, Company company);
+    List<Recruitment> findAll(Specification<Recruitment> word);
 }
