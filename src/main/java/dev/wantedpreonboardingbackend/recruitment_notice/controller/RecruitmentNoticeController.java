37ea140ed87fd36106a4ce75f1aee23744018f04
soonhankwon <soonable@gmail.com>
2023-10-06 16:20:40 +0900

    feat : RecruitmentNoticeController (RestController)

--- src/main/java/dev/wantedpreonboardingbackend/recruitment_notice/controller/RecruitmentNoticeController.java
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment_notice/controller/RecruitmentNoticeController.java
@@ -1,2 +1,7 @@
-package dev.wantedpreonboardingbackend.recruitment_notice.controller;public class RecruitmentNoticeController {
+package dev.wantedpreonboardingbackend.recruitment_notice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecruitmentNoticeController {
 }
