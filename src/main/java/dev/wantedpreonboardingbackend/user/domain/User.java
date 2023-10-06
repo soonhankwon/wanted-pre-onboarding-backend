8eee15f24452dbc73f7d438a60bee55e48880e19
soonhankwon <soonable@gmail.com>
2023-10-06 17:00:31 +0900

    feat : EqualsAndHashCode 추가 & 이메일검증 메서드 + 생성자에 적용(#1)

--- src/main/java/dev/wantedpreonboardingbackend/user/domain/User.java
+++ src/main/java/dev/wantedpreonboardingbackend/user/domain/User.java
@@ -1,16 +1,34 @@
 package dev.wantedpreonboardingbackend.user.domain;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
 
 @NoArgsConstructor
 @Entity
+@EqualsAndHashCode
 @Table(name = "`user`")
 public class User {
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String email;
+
+    public User(String email) {
+        if (isValidEmail(email)) {
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException("이메일 형식에 맞지 않습니다.");
+        }
+    }
+
+    private boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
 }
