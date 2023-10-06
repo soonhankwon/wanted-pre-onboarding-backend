4302dce567576e8939c665865b5d3115bc3a034b
soonhankwon <soonable@gmail.com>
2023-10-06 16:19:30 +0900

    feat : recruitment_notice package domain 모델링(#1)

--- src/main/java/dev/wantedpreonboardingbackend/recruitment_notice/domain/RecruitmentNotice.java
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment_notice/domain/RecruitmentNotice.java
@@ -1,2 +1,26 @@
-package dev.wantedpreonboardingbackend.recruitment_notice.domain;public class RecruitmentNotice {
+package dev.wantedpreonboardingbackend.recruitment_notice.domain;
+
+import dev.wantedpreonboardingbackend.tech.domain.Tech;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@Entity
+public class RecruitmentNotice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String position;
+
+    private Long compensation;
+
+    private String description;
+
+    @OneToMany(mappedBy = "recruitmentNotice", cascade = CascadeType.ALL)
+    private Set<Tech> requiredTechs = new HashSet<>();
 }
