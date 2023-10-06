3ac35e7dd2c30b5abca885a1f9f171f689f808df
soonhankwon <soonable@gmail.com>
2023-10-06 19:30:39 +0900

    chore : rename method

--- src/test/java/dev/wantedpreonboardingbackend/user/domain/UserTest.java
+++ src/test/java/dev/wantedpreonboardingbackend/user/domain/UserTest.java
@@ -10,21 +10,21 @@ class UserTest {
 
     @Test
     @DisplayName("User NoArgs 테스트")
-    void createUser_NoArgs() {
+    void createUser_noArgs() {
         User user = new User();
         assertThat(user).isNotNull();
     }
 
     @Test
     @DisplayName("User 생성자 테스트 & EqualsAndHashCode")
-    void createUser_Constructor_EqualsAndHashCode() {
+    void createUser_constructor_equalsAndHashCode() {
         User user = new User("abc@naver.com");
         assertThat(user).isEqualTo(new User("abc@naver.com"));
     }
 
     @Test
     @DisplayName("User 생성시 유효하지 않은 이메일 검증")
-    void validateEmail_Constructor() {
+    void validateEmail_constructor() {
         assertThatThrownBy(() -> new User("abcnaver.com"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
