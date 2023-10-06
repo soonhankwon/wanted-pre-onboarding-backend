f596ee56b059c0d0a05948b3e8e685b0c157a56a
soonhankwon <soonable@gmail.com>
2023-10-06 18:57:02 +0900

    feat : 채용공고 수정 API - 컨트롤러(#3)

--- src/main/java/dev/wantedpreonboardingbackend/recruitment/controller/RecruitmentController.java
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment/controller/RecruitmentController.java
@@ -1,6 +1,7 @@
 package dev.wantedpreonboardingbackend.recruitment.controller;
 
 import dev.wantedpreonboardingbackend.recruitment.controller.dto.RecruitmentRegisterRequest;
+import dev.wantedpreonboardingbackend.recruitment.controller.dto.RecruitmentUpdateRequest;
 import dev.wantedpreonboardingbackend.recruitment.service.RecruitmentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,8 +18,16 @@ public class RecruitmentController {
 
     @PostMapping("/{companyId}")
     public ResponseEntity<?> registerRecruitment(@PathVariable Long companyId,
-                                              @RequestBody RecruitmentRegisterRequest dto) {
+                                                 @RequestBody RecruitmentRegisterRequest dto) {
         regularRecruitmentService.registerRecruitment(companyId, dto);
         return ResponseEntity.ok().body("채용공고 등록완료");
+    }
+
+    @PatchMapping("/{recruitmentId}/{companyId}")
+    public ResponseEntity<?> updateRecruitment(@PathVariable Long recruitmentId,
+                                               @PathVariable Long companyId,
+                                               @RequestBody RecruitmentUpdateRequest dto) {
+        regularRecruitmentService.updateRecruitment(recruitmentId, companyId, dto);
+        return ResponseEntity.ok().body("채용공고 수정완료");
     }
 }
