385e17a4bd0314e6bc7fce09b6a23928a038d6fe
soonhankwon <soonable@gmail.com>
2023-10-06 18:57:45 +0900

    feat : 채용공고 id와 회사 fk로 조회(#3)

--- src/main/java/dev/wantedpreonboardingbackend/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment/repository/RecruitmentRepository.java
@@ -1,9 +1,13 @@
 package dev.wantedpreonboardingbackend.recruitment.repository;
 
+import dev.wantedpreonboardingbackend.company.domain.Company;
 import dev.wantedpreonboardingbackend.recruitment.domain.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+    Optional<Recruitment> findByIdAndCompany(Long id, Company company);
 }
