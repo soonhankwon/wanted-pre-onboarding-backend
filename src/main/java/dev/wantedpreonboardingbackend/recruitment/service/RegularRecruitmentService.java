e29f89cba90141596169024427480f7b01998194
soonhankwon <soonable@gmail.com>
2023-10-06 18:58:48 +0900

    feat : 채용공고 수정 비즈니스 로직(#3)

--- src/main/java/dev/wantedpreonboardingbackend/recruitment/service/RegularRecruitmentService.java
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment/service/RegularRecruitmentService.java
@@ -5,10 +5,12 @@ import dev.wantedpreonboardingbackend.company.repository.CompanyRepository;
 import dev.wantedpreonboardingbackend.exception.ApiException;
 import dev.wantedpreonboardingbackend.exception.CustomErrorCode;
 import dev.wantedpreonboardingbackend.recruitment.controller.dto.RecruitmentRegisterRequest;
+import dev.wantedpreonboardingbackend.recruitment.controller.dto.RecruitmentUpdateRequest;
 import dev.wantedpreonboardingbackend.recruitment.domain.Recruitment;
 import dev.wantedpreonboardingbackend.recruitment.repository.RecruitmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,10 +20,22 @@ public class RegularRecruitmentService implements RecruitmentService {
     private final CompanyRepository companyRepository;
 
     @Override
+    @Transactional
     public void registerRecruitment(Long companyId, RecruitmentRegisterRequest dto) {
         Company company = companyRepository.findById(companyId)
                 .orElseThrow(() -> new ApiException(CustomErrorCode.COMPANY_NOT_FOUND_INVALID_ID));
         Recruitment recruitment = new Recruitment(dto, company);
         recruitmentRepository.save(recruitment);
+    }
+
+    @Override
+    @Transactional
+    public void updateRecruitment(Long recruitmentId, Long companyId, RecruitmentUpdateRequest dto) {
+        Company company = companyRepository.findById(companyId)
+                .orElseThrow(() -> new ApiException(CustomErrorCode.COMPANY_NOT_FOUND_INVALID_ID));
+        Recruitment recruitment = recruitmentRepository.findByIdAndCompany(recruitmentId, company)
+                .orElseThrow(() -> new ApiException(CustomErrorCode.RECRUITMENT_NOT_MATCHES_COMPANY_ID));
+
+        recruitment.update(dto);
     }
 }
