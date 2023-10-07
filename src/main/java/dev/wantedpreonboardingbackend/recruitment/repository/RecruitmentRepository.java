9016abbbcde6bed839dfb2fe9d9c594276671537
soonhankwon <soonable@gmail.com>
2023-10-07 14:53:30 +0900

    feat : custom 쿼리 인터페이스 상속 - 기존 RecruitmentRepository 로 커스텀쿼리를 사용가능하도록 구현(#6)

--- src/main/java/dev/wantedpreonboardingbackend/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment/repository/RecruitmentRepository.java
@@ -2,15 +2,12 @@ package dev.wantedpreonboardingbackend.recruitment.repository;
 
 import dev.wantedpreonboardingbackend.company.domain.Company;
 import dev.wantedpreonboardingbackend.recruitment.domain.Recruitment;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long>, RecruitmentRepositoryCustom {
     Optional<Recruitment> findByIdAndCompany(Long id, Company company);
-    List<Recruitment> findAll(Specification<Recruitment> word);
 }
