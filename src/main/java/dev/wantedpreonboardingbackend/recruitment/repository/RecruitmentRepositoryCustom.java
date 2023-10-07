96e8aab0b35660ab098a8b07f7ae5a128fb8f634
soonhankwon <soonable@gmail.com>
2023-10-07 14:51:20 +0900

    feat : querydsl 사용 -커스텀쿼리 repository 인터페이스(#6)

---
+++ src/main/java/dev/wantedpreonboardingbackend/recruitment/repository/RecruitmentRepositoryCustom.java
@@ -0,0 +1,9 @@
+package dev.wantedpreonboardingbackend.recruitment.repository;
+
+import dev.wantedpreonboardingbackend.recruitment.domain.Recruitment;
+
+import java.util.List;
+
+public interface RecruitmentRepositoryCustom {
+    List<Recruitment> findRecruitmentsByKeyword(String keyword);
+}
